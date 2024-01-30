4cb93993a7d914e1bf6c203e148cc9c827b744fc
Tara <hymerstara@gmail.com>
2024-01-29 18:31:11 -0700

    commit the function

--- play.js
+++ play.js
@@ -1,29 +1,5 @@
-const net = require("net");
+// Import the connect function from client.js
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', 
-    port: 50541, 
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Event handler for successful connection
-  conn.on('connect', () => {
-    console.log('Connected to the server!');
-
-  });
-
-  // Event handler for incoming data
-  conn.on('data', (data) => {
-    console.log('Server says:', data);
-
-  });  
-
-  return conn;
-};
-
-console.log("Connecting ...");
+// Call the connect function
 connect();
