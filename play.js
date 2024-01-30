5e1f3c244da3b17c6c346109f7b1e56e30848892
Tara <hymerstara@gmail.com>
2024-01-29 18:57:54 -0700

    setup stdin

--- play.js
+++ play.js
@@ -3,3 +3,23 @@ const connect = require('./client');
 
 // Call the connect function
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  console.log("User pressed:", key);
+
+// If 'Ctrl+C' is pressed, exit the process
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
