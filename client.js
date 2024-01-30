3e9f3b811fc4ca73a00c10436553e3c4acc283e1
Tara <hymerstara@gmail.com>
2024-01-29 18:31:57 -0700

    export the module

---
+++ client.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost', 
+    port: 50541, 
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Event handler for successful connection
+  conn.on('connect', () => {
+    console.log('Connected to the server!');
+
+  });
+
+  // Event handler for incoming data
+  conn.on('data', (data) => {
+    console.log('Server says:', data);
+
+  });  
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
